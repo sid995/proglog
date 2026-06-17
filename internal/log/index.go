febc1a9f5f7deca21036d7c7abaa47527180e499
Siddharth Kundu <sk31395@gmail.com>
2026-06-17 19:41:09 +0530

    index section progress

--- internal/log/index.go
+++ internal/log/index.go
@@ -1,11 +1,13 @@
 package log
 
 import (
+	"io"
 	"os"
 
 	"github.com/tysonmote/gommap"
 )
 
+// the width constants define the number of bytes that make up each index entry
 var (
 	offWidth uint64 = 4
 	posWidth uint64 = 8
@@ -18,6 +20,10 @@ type index struct {
 	size uint64
 }
 
+// create index and save the current size of the file so we
+// can track the amount of data in the index file as we add index entries
+// Grow the file to max index size before memory-mapping the file and then
+// returning the created index to the caller
 func newIndex(f *os.File, c Config) (*index, error) {
 	idx := &index{
 		file: f,
@@ -45,6 +51,45 @@ func newIndex(f *os.File, c Config) (*index, error) {
 	return idx, nil
 }
 
+// Read takes in an offset and returns the associated record's position in the store.
+// The given offset is relative to the segment's base offset (0 is always the offset of the index's first entry
+func (i *index) Read(in int64) (out uint32, pos uint64, err error) {
+	if i.size == 0 {
+		return 0, 0, io.EOF
+	}
+	if in == -1 {
+		out = uint32((i.size / entWidth) - 1)
+	} else {
+		out = uint32(in)
+	}
+	out = enc.Uint32(i.mmap[pos : pos+offWidth])
+	pos = enc.Uint64(i.mmap[pos+offWidth : pos+entWidth])
+	return out, pos, nil
+}
+
+// Write() appends the given offset and position to the index.
+// Validate that we have space to write entry.
+// If there is space, encode the offset and position and
+// write them to the memory-mapped file.
+// Then we increment the position where the next write will go.
+func (i *index) Write(off uint32, pos uint64) error {
+	if uint64(len(i.mmap)) < i.size+entWidth {
+		return io.EOF
+	}
+	enc.PutUint32(i.mmap[i.size:i.size+offWidth], off)
+	enc.PutUint64(i.mmap[i.size+offWidth:i.size+entWidth], pos)
+	i.size += uint64(entWidth)
+	return nil
+}
+
+func (i *index) Name() string {
+	return i.file.Name()
+}
+
+// Close makes sure the memory-mapped file has synced its data to the persisted
+// file and that the persisted file has flushed its contents to stable storage
+// Then it truncates the persisted file to the amount of data that’s
+// actually in it and closes the file
 func (i *index) Close() error {
 	if err := i.mmap.Sync(gommap.MS_SYNC); err != nil {
 		return err
